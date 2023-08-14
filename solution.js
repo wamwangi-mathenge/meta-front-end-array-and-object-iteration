d6ba576ffb56a969f895d2f8cf7c1e333c36b4ee
Brian <wamwangimathenge@gmail.com>
2023-08-14 15:47:29 +0300

    Solve Task 1
    This commit creates a function logDairy. Within it, each of the items
    in the dairy array are displayed.

---
+++ solution.js
@@ -0,0 +1,12 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy() {
+    for (var d of dairy) {
+        console.log(d)
+    }
+}
+logDairy()
+// Task 2
+
+// Task 3
